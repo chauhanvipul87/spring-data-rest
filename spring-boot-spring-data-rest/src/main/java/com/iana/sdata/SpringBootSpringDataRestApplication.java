639171ad3df85638573efe5211c5a21989fc5a98
vipul Chauhan <test.jpatel1@gmail.com>
2017-10-29 20:48:19 +0530

    Spring Boot + Spring Data Rest + H2

---
+++ spring-boot-spring-data-rest/src/main/java/com/iana/sdata/SpringBootSpringDataRestApplication.java
@@ -0,0 +1,12 @@
+package com.iana.sdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSpringDataRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSpringDataRestApplication.class, args);
+	}
+}
